6f9260bb89f65ef076ae9622d8ca3b7373c533f2
your_github_username <rabi.abdelrahman@gmail.com>
2020-11-15 22:52:51 +0200

    test

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,37 +1,17 @@
-
-import React, { useState, useEffect } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-} from 'react-router-dom';
-import './App.css';
-import Post from './components/Post';
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./App.css";
+import Post from "./components/Post";
 import Register from "./components/Register";
 import Login from "./components/Login";
 
-
 const App = () => {
- 
   return (
-      <div>
-        <Post/>
-      </div>
-   
-  
-  )
-};
-export default App;
-
     <div>
       <Register />
       <Login />
+      <Post />
     </div>
   );
 };
 export default App;
-
-
-
